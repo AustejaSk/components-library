dbd3de910e3dfcd6a45ee4679b437145394fc713
Austeja <sketeryteausteja@gmail.com>
2024-06-29 16:52:04 +0200

    update index.jsx & components-styles.css

--- index.jsx
+++ index.jsx
@@ -8,7 +8,7 @@ function App() {
     <>
       <div className="header">
         <h1>Component Library</h1>
-        <a href="https://github.com/AustejaSk/components-library"><img src={githubIcon} /></a>
+        <a href="https://github.com/AustejaSk/components-library" target='_blank'><img src={githubIcon} /></a>
       </div>
       {componentsExample}
     </>
